42fe32fee706656a1aeb4f6d0cef24f35408e555
Jonas Henriksson <jonas.henriksson@hiq.se>
2019-02-12 13:32:17 +0100

    first commit

---
+++ src/main/java/se/hiq/workoutrestservices/WorkoutRestServicesApplication.java
@@ -0,0 +1,14 @@
+package se.hiq.workoutrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutRestServicesApplication.class, args);
+	}
+
+}
+
