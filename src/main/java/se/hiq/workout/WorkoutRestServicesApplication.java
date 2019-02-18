4d2c105166c5531cb575f5257a632cbfe21ec76a
Ernesto Garcia <ernesto.garcia@hiq.se>
2019-02-18 08:39:59 +0100

    Added CORS configuration.

--- src/main/java/se/hiq/workout/WorkoutRestServicesApplication.java
+++ src/main/java/se/hiq/workout/WorkoutRestServicesApplication.java
@@ -2,12 +2,25 @@ package se.hiq.workout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WorkoutRestServicesApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WorkoutRestServicesApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*").allowedOrigins("*");
+      }
+    };
   }
 
 }
