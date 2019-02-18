405107ed6412017a84e7d82fba70ecf080626853
Jonas Henriksson <jonas.henriksson@hiq.se>
2019-02-18 15:51:31 +0100

    updated pipeline

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,26 +1,14 @@
-#!/usr/bin/env bash
+echo "starting awesome react docker deployment script"
+echo "start by building new backend java image"
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
+docker stop workout-back
 
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-set +x
+docker build -t workout-rest .
 
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
+echo "start new rest container"
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+JENKINS_NODE_COOKIE=dontkillme2 docker run -d  \
+  --name workout-back \
+  -p 8082:8081 \
+  --rm \
+  workout-rest
